6b83755148835a6a24c194a6a016e402f31e75a6
luis <lesc719@aucklanduni.ac.nz>
2016-09-29 17:41:21 +1300

    tidying data files

---
+++ temp.py
@@ -0,0 +1,6 @@
+city_list = ['bom','akl','syd']
+city_name = input('Enter the city name:')
+if city_name in city_list:
+    print ('City found')
+else:
+    print ('city not found')
---
+++ untitled.py
@@ -0,0 +1,3 @@
+file_obsidian = open('obsidian123-artefact-cleandata.csv')
+data = file_obsidian.read()
+print(data)
